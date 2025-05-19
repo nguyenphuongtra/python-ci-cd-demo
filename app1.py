e68759114591c9551adb51dd0a3a0f679eb4215c
Your Name <you@example.com>
2025-05-20 00:30:55 +0700

    Add Docker CD pipeline

--- app1.py
+++ app1.py
@@ -1,9 +1,10 @@
-from flask import Flask
+"""Module này chứa Flask app."""
 
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
+    """Trang chủ trả về chuỗi chào mừng."""
     return "Hello from Flask CI/CD on Heroku!"
