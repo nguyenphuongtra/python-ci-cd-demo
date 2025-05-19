96b775e9a6bf654e5fdc9cbd1a691d1e0e8a8fe7
Your Name <you@example.com>
2025-05-20 00:01:48 +0700

    Fix flake8 E302 by adding blank lines before functions

--- test_app.py
+++ test_app.py
@@ -1,7 +1,15 @@
-"""Module chứa các test cho app."""
-
 from app import add
+from app1 import app
+
 
 def test_add():
     """Test hàm add."""
     assert add(1, 2) == 3
+
+
+def test_home():
+    """Test trang chủ."""
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.data == b'Hello from Flask CI/CD on Heroku!'
