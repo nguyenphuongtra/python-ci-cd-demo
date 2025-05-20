aa66976567008c84f2ebc844287f5cae0ebba9ea
Your Name <you@example.com>
2025-05-20 10:04:51 +0700

    First commit

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 5000
 
 # Start the app
-CMD ["python", "app.py"]
+CMD ["python", "app1.py"]
 
 
 
--- app1.py
+++ app1.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """Trang chủ trả về chuỗi chào mừng."""
-    return "Hello from Flask CI/CD on Heroku!"
+    return "Hello from Flask CI/CD on DockerHub!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
